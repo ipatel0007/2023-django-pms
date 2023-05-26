30ca443fbac40eef2fbe4c701309c3f7a750acda
Ashutosh Patel <ipateldesigndevs1@gmail.com>
2023-05-26 09:14:23 +0530

    SETTINGS COMMIT

--- pms/pms/settings.py
+++ pms/pms/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True  
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'patelap871@gmail.com'
-EMAIL_HOST_PASSWORD = 'wncvhjltjfjsowpd'
+EMAIL_HOST_USER = 'YOUR EMAIL'
+EMAIL_HOST_PASSWORD = 'YOUR PASSWORD'
